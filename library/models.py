e447279e1776b53c2583cf3f5bf99b33f89727ff
Claudio Guevara <claudio.guevara.dev@gmail.com>
2025-10-22 20:58:15 -0300

    models for library ready

--- library/models.py
+++ library/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="items/")
+    categories = models.ManyToManyField(Category, related_name="items", blank=True)
+    url = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
