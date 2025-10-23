591f49f4f57d3af8cfceb14f30f4ceae2dc5a77b
Claudio Guevara <claudio.guevara.dev@gmail.com>
2025-10-23 10:51:55 -0300

    grid configurado

--- library/models.py
+++ library/models.py
@@ -2,16 +2,20 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="items/")
+    description = models.CharField(max_length=150)
+    image = models.ImageField(upload_to="items/", blank=True, null=True)
     categories = models.ManyToManyField(Category, related_name="items", blank=True)
     url = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self):
         return self.name
--- library/views.py
+++ library/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from math import ceil
+
+from .models import Category, Item
 
 def home(request):
-    context = {}
+    categories = Category.objects.all()
+    items = Item.objects.all()
+    latest_items = Item.objects.all().order_by('-created_at')[:9]
+
+    search = request.GET.get("search", "")
+    category = request.GET.get("category", "")
+
+    if search:
+        items = items.filter(name__icontains=search) | items.filter(description__icontains=search)
+
+    if category:
+        items = items.filter(categories__id=category)
+
+    paginator = Paginator(items, 4)
+    page_number = request.GET.get("page")
+    items = paginator.get_page(page_number)
+
+    slides = [latest_items[i:i+3] for i in range(0, len(latest_items), 3)]
+
+    context = {
+        "categories": categories,
+        "items": items,
+        "latest_items": latest_items,
+        "slides": slides
+    }
 
     return render(request, "index.html", context)
