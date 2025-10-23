71cedeaf1c6dec3b15c0c912360c56cb71598a67
Claudio Guevara <claudio.guevara.dev@gmail.com>
2025-10-23 17:59:26 -0300

    django secret

--- chichalibrary/settings.py
+++ chichalibrary/settings.py
@@ -1,6 +1,7 @@
 import os
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6l@75u43h@tf^(b)e!1hnzsvb177gxzp*q&8qlnz94%$zic22m'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
